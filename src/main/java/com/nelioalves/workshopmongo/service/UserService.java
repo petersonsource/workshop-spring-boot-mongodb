7bc7c55bc0b4fd94ece539140e69c9e25db3ae5a
Peterson Santos <peterson.opensource@gmail.com>
2023-11-26 21:23:13 -0300

    'obtendo usuario por id

--- src/main/java/com/nelioalves/workshopmongo/service/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.nelioalves.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,14 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		if( !user.isPresent() ) {
+			throw new ObjectNotFoundException("objeto não encontrado");
+		} 
+		return user.get();
 	}
 	
 }
