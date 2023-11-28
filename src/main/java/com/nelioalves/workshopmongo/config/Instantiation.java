91ef55f9e44d8e311b232b21614129d1ad4081d7
Peterson Santos <peterson.opensource@gmail.com>
2023-11-27 22:18:39 -0300

    criando entity post com user aninhado

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.nelioalves.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mongodb.client.model.geojson.Position;
+import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,17 +20,29 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");	
+		User rick = new User(null, "Rick James", "rj@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", maria);
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", maria);
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, rick));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 	
