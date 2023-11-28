fafc6586b314c4246eb8535130718879a0dda90a
Peterson Santos <peterson.opensource@gmail.com>
2023-11-27 23:14:01 -0300

    obtendo um post por id

---
+++ src/main/java/com/nelioalves/workshopmongo/service/PostService.java
@@ -0,0 +1,26 @@
+package com.nelioalves.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		if( !post.isPresent() ) {
+			throw new ObjectNotFoundException("objeto não encontrado");
+		} 
+		return post.get();
+	}
+	
+}
